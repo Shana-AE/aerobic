2ae911754cf6ffa7913354b13fc9ab89fa1e9562
ShanaAE <Shana.wav@gmail.com>
2020-07-05 22:10:41 +0800

    add animation to the submenu using react-transition-group

--- src/components/Menu/SubMenu.tsx
+++ src/components/Menu/SubMenu.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState, FunctionComponentElement } from 'react';
 import classNames from 'classnames';
+import { CSSTransition } from 'react-transition-group';
 
 import { MenuContext } from './Menu';
 import { MenuItemProps } from './MenuItem';
@@ -80,7 +81,17 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
         );
       }
     });
-    return <ul className={subMenuClasses}>{childrenComponent}</ul>;
+    return (
+      <CSSTransition
+        in={menuOpen}
+        timeout={300}
+        classNames='zoom-in-top'
+        appear
+        unmountOnExit
+      >
+        <ul className={subMenuClasses}>{childrenComponent}</ul>
+      </CSSTransition>
+    );
   };
 
   return (
